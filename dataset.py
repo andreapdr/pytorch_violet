b8000b731e3da87d3d16bda6206bd182eba6b62c
andreapdr <andrea.pedrotti@isti.cnr.it>
2023-03-22 17:33:46 +0100

    adapted to json format

--- dataset.py
+++ dataset.py
@@ -1,9 +1,6 @@
-from collections import Counter
 import torch
-import av 
 import torchvision
 from torchvision.transforms import ToPILImage
-from av import VideoFrame
 
 from lib import *
 
@@ -36,109 +33,6 @@ class Dataset_Base(T.utils.data.Dataset):
         return txt, mask
 
 
-class FoilingDataset(Dataset_Base):
-    def __init__(
-        self,
-        args,
-        split,
-        tasks=["action_foil", "preState_foil", "postState_foil", "reverse_foil"],
-        debug=False,
-    ):
-        super().__init__(args)
-
-        self.datasetName = args["dataset"]
-        self.tasks = ["capt"] + tasks
-        if "all" in args["dataset"].lower():
-            _allDataset = ["coin", "rareAct", "youCook2"]  # TODO: "smsm"
-            self.img = {
-                k: pickle.load(open(f"_data/videos/{k}.pkl", "rb")) for k in _allDataset
-            }
-        else:
-            self.img = pickle.load(open(f'_data/videos/{args["dataset"]}.pkl', "rb"))
-        self.txt = json.load(open(f'./_data/foils/{args["dataset"]}.json', "r"))[split]
-        self.keys = list(self.txt.keys())
-        self.debug = False
-        if debug:
-            self.debug = True
-            print("- NB: Running DATASET IN DEBUG MODE (foil sentence is hard-typed")
-
-    def __len__(self):
-        return len(self.keys)
-
-    def set_view(self, trigger=None, task=None):
-        new_triggers = []
-        new_tasks = []
-        if trigger is not None:
-            for k in self.keys:
-                if self.txt[k]["verb"] == trigger:
-                    new_triggers.append(k)
-                if task is not None:
-                    if self.txt[k]["linguistic_phenomena"] == task:
-                        new_tasks.append(k)
-        if task is None:
-            new_tasks = self.keys
-
-        new_list = list(set(new_triggers).intersection(set(new_tasks)))
-        _task = "all" if task is None else task
-        print(
-            f"New view (trigger only={trigger}, task only={_task}) - len: {len(new_list)}"
-        )
-        self.keys = new_list
-
-    def get_trigger_list(self):
-        triggers = [
-            self.txt[k]["verb"]
-            for k in self.keys
-            if self.txt[k]["setting"] == "action recognition (LLM)"
-        ]
-        counter = self._set_counter(triggers)
-        return sorted(
-            [(k, v) for k, v in counter.items()], key=lambda x: x[1], reverse=True
-        )
-
-    def _set_counter(self, triggers):
-        self.counter = Counter(triggers)
-        return self.counter
-
-    def __getitem__(self, idx):
-        key_dict = self.keys[idx]
-        item = self.txt[key_dict]
-
-        img = []
-        video_name = (
-            f"{item['youtube_id']}_{int(item['start_time'])}_{int(item['end_time'])}"
-        )
-        if "all" in self.datasetName:
-            buffer = self.img[item["original_dataset"]].get(video_name, None)
-        elif "smsm" in item["original_dataset"]:
-            buffer = self.img.get(video_name.replace("_0_0", ""), None)
-        else:
-            buffer = self.img.get(video_name, None)
-        if buffer is None:
-            return None, None, key_dict, item
-        for b in buffer:
-            img.append(self.str2img(b).unsqueeze(0))
-        img = T.cat(img, dim=0)
-        texts = {k: self.str2txt(item[k]) for k in self.tasks}
-        return img, texts, key_dict, item
-
-
-class FoilConcatDataset(T.utils.data.ConcatDataset):
-    def __init__(self, datasets):
-        super().__init__(datasets)
-        self.tokzr = self.datasets[0].tokzr
-
-    def get_trigger_list(self):
-        for d in self.datasets:
-            d.get_trigger_list()
-        self.counter = sum(
-            [d.counter for d in self.datasets], start=Counter()
-        )  # https://stackoverflow.com/questions/30003466/summing-list-of-counters-in-python
-        return sorted(
-            [(k, v) for k, v in self.counter.items()], key=lambda x: x[1], reverse=True
-        )
-
-
 class VLBenchDataset(Dataset_Base):
     def __init__(self, datapath, args):
         super().__init__(args)
@@ -162,11 +56,14 @@ class VLBenchDataset(Dataset_Base):
     
     def _convert(self, d):
         d = torch.tensor(d)
-        # d = d.unsqueeze(0)
         return d
     
     def _get_video(self, idx):
-        video_path = os.path.join(self.videodir, self.data[idx]["youtube_id"]) + ".mp4"
+        if self.data[idx]["youtube_id"] is not None:
+            video_fname = self.data[idx]["youtube_id"] + ".mp4"
+        else:
+            video_fname = self.data[idx]["video_file"]  # TODO: what about extension?
+        video_path = os.path.join(self.videodir, video_fname)
         start_time = self.data[idx]["start_time"]
         end_time = self.data[idx]["end_time"]
         sampled_frames = self._process_video(video_path, start_time, end_time)
@@ -175,9 +72,7 @@ class VLBenchDataset(Dataset_Base):
             s = s.permute(2, 0, 1)
             s = ToPILImage()(s).convert("RGB")
             imgs.append(self._vlbench_str2img(s))
-        # img = T.cat(imgs, dim=0)
-        img = T.stack(imgs) # TODO
-        # return img.unsqueeze(0)
+        img = T.stack(imgs) # TODO: check this once again
         return img
 
     def __getitem__(self, idx):
